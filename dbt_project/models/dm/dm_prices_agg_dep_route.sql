6306c1aa027f8c006ed48751b4a455acca52fa5a
Teplyakov Nikolay <nikolay.tep@yandex.ru>
2025-12-13 12:53:56 +0300

    add ipynb

--- dbt_project/models/dm/dm_prices_agg_dep_route.sql
+++ dbt_project/models/dm/dm_prices_agg_dep_route.sql
@@ -1,14 +1,14 @@
-{{ config(
-    materialized = 'table',
-    schema = 'dm',
-    tags = ['dm']
-) }}
+    {{ config(
+        materialized = 'table',
+        schema = 'dm',
+        tags = ['dm']
+    ) }}
 
-select
-    departure_date,
-    any_value(departure_weekday_short) as departure_weekday,
-    origin,
-    destination,
-    avg(price_rub) as avg_price_rub
-from {{ ref('dm_fact_ticket_offers')}}
-group by departure_date, origin, destination
+    select
+        departure_date,
+        any_value(departure_weekday_short) as departure_weekday,
+        origin,
+        destination,
+        avg(price_rub) as avg_price_rub
+    from {{ ref('dm_fact_ticket_offers')}}
+    group by departure_date, origin, destination
